21e04f6a42ed78585c4ad5ce94d957578f0ed6fc
dhruvnps <dhruv.pattem@gmail.com>
2021-09-28 19:39:32 +0100

    init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>Installed CLI Plugins</h3>
+    <h3>Ecosystem</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: {},
+};
+</script>
+
+<style scoped>
+</style>
