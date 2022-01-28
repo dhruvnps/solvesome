0bf930c162d5bc0aea2ab31f049e30d7356b05e9
dhruvnps <dhruv.pattem@gmail.com>
2022-01-28 10:18:37 +0000

    auth guard fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,25 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import Login from '@/views/Login.vue';
+import Home from '@/views/Home.vue';
 import { auth } from "@/firebase";
 
 const routes = [
   {
     path: '/login',
     name: 'Login',
-    component: Login
+    component: Login,
+    meta: { auth: true },
   },
   {
     path: '/signup',
     name: 'Signup',
-    component: Login
+    component: Login,
+    meta: { auth: true },
   },
   {
     path: '/',
     name: 'Home',
-    component: () => import('@/views/Home.vue'),
+    component: Home,
     meta: { authRequired: true },
   },
   {
@@ -40,17 +43,13 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.authRequired)) {
-    auth.onAuthStateChanged(function (user) {
-      if (user) {
-        next();
-      } else {
-        next({ path: '/login' });
-      }
-    });
-  } else {
-    next();
-  }
+  auth.onAuthStateChanged(function (user) {
+    if (to.meta.authRequired) {
+      next(user ? {} : { path: '/login' });
+    } else if (to.meta.auth) {
+      next(user ? { path: '/' } : {});
+    }
+  });
 });
 
 export default router;
